b51a4511aa1c6708fbf98d394ada8b6bd07ac6fb
Abhishek Thrormothe <117808499+abhi2k4@users.noreply.github.com>
2024-01-28 18:08:57 +0530

    new index added

--- scripts.js
+++ scripts.js
@@ -124,3 +124,38 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
+var countDownDate = new Date("Feb 26, 2024 00:00:00").getTime();
+var x = setInterval(function() {
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+}, 1000);
+    const observerOptions = {
+    threshold: 0.5, 
+  };
+  const handleIntersection = (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        document.querySelector('.left-to-right .ball').style.animationPlayState = 'running';
+        document.querySelector('.right-to-left .ball').style.animationPlayState = 'running';
+      } else {
+        document.querySelector('.left-to-right .ball').style.animationPlayState = 'paused';
+        document.querySelector('.right-to-left .ball').style.animationPlayState = 'paused';
+      }
+    });
+  };
+
+  const observer = new IntersectionObserver(handleIntersection, observerOptions);
+
+  const targetElement = document.querySelector('.preGallerySection');
+
+  observer.observe(targetElement);
