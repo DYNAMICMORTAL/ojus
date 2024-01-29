f2492085b4d4f036c5c167eedc7892c49b7939e6
Mihir <aminvasudev6@gmail.com>
2024-01-29 23:23:45 +0530

    About Section Landing Page Desktop Version 3

--- script2.js
+++ script2.js
@@ -17,8 +17,9 @@ var x = setInterval(function() {
     threshold: 0.5, 
   };
 
-//   document.addEventListener("DOMContentLoaded", function() {
-//     setTimeout(function() {
-//         document.querySelector('.preloader').style.display = 'none';
-//     }, 13000); // 8000 milliseconds (8 seconds)
-// });
+document.addEventListener("DOMContentLoaded", function () {
+  setTimeout(function () {
+      document.querySelector('.preloader').style.display = 'none';
+      document.querySelector('#home-section').style.display = 'block';
+  }, 4000);
+});
