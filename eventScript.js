af5d3871060874eec13508d73f2c015b64a41810
Mihir <aminvasudev6@gmail.com>
2024-02-10 11:33:11 +0530

    Events v3.0.0

--- eventScript.js
+++ eventScript.js
@@ -1,25 +1,16 @@
-// const paintBall = document.querySelector('.paintBall');
-//         let lastScrollY = window.scrollY || window.pageYOffset;
-//         window.addEventListener('scroll', function() {
-//             const scrollY = window.scrollY || window.pageYOffset;
-//             if (scrollY > lastScrollY) {
-//                 console.log('scrolling down');
-//                 paintBall.style.top = `${parseInt(paintBall.style.top || 0) - 1}px`;
-//             } else {
-//                 console.log('scrolling up');
-//                 paintBall.style.top = `${parseInt(paintBall.style.top || 0) + 1}px`;
-//             }
-//             console.log('scrollY: ', scrollY);
-//             lastScrollY = scrollY;
-//         });
 const paintBall = document.querySelector('.paintBall');
 let lastScrollY = window.scrollY || window.pageYOffset;
-
 window.addEventListener('scroll', function() {
     const scrollY = window.scrollY || window.pageYOffset;
     const deltaY = scrollY - lastScrollY;
-    const movementAmount = deltaY / 6;
-    console.log('movementAmount: ', movementAmount);
-    paintBall.style.top = `${parseInt(paintBall.style.top || 0) - movementAmount}px`;
+    const movementAmount = deltaY / 4;
+    const boundingBox = paintBall.getBoundingClientRect();
+    const threshold = window.innerHeight + 100;
+    if (boundingBox.bottom < -threshold || boundingBox.top > threshold) {
+        paintBall.style.top = '0px';
+    } else {
+        paintBall.style.top = `${parseInt(paintBall.style.top || 0) - movementAmount}px`;
+    }
+    
     lastScrollY = scrollY;
 });
